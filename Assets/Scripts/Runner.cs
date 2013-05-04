0bf79a9f8fb8766bcd05603e8dad9c00d1f113a4
Adam Proctor <aproctor@gmail.com>
2013-05-04 12:46:08 -0400

    don't start running till the game begins

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -11,19 +11,46 @@ public class Runner : MonoBehaviour {
 
 	private bool touchingPlatform;
 	private int jumpCount = 0;
+	private Vector3 startPosition;
 	
-	void Start() {
+	private enum playerStates {Idle, Running, Dead};
+	private playerStates playerState;
+
+	void Start () {
+		GameEventManager.GameStart += GameStart;
+		startPosition = transform.localPosition;
+		playerState = playerStates.Idle;
+	}	
+		
+	private void GameStart () {
+		distanceTraveled = 0f;
+		transform.localPosition = startPosition;
+		rigidbody.isKinematic = false;
+		enabled = true;
 		jumpCount = 0;
+		playerState = playerStates.Running;
+	}
+
+	private void GameOver () {
+		rigidbody.isKinematic = true;
+		enabled = false;
+		playerState = playerStates.Dead;
 	}
 
 	void Update () {
-		if(Input.GetButtonDown("Jump")){
-			Jump();
+		if(playerState == playerStates.Running) {
+			if(Input.GetButtonDown("Jump")){
+				Jump();
+			}		
+			distanceTraveled = transform.localPosition.x;
 		}
-		distanceTraveled = transform.localPosition.x;
 	}
 	
 	private void Jump() {
+		if(distanceTraveled < 5) {
+			//Too early to jump
+			return;
+		}
 		var canJump = false;
 		if(touchingPlatform) {
 			canJump = true;
@@ -32,17 +59,19 @@ public class Runner : MonoBehaviour {
 			//TODO super power
 		}
 		if(canJump) {
-			Debug.Log("JUMPING");
 			rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
 			jumpCount += 1;
 		} else {
 			Debug.Log("D'Oh");
+			//Jump fail
 		}
 	}
 
 	void FixedUpdate () {
-		if(touchingPlatform){
-			rigidbody.AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
+		if(playerState == playerStates.Running) {
+			if(touchingPlatform){
+				rigidbody.AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
+			}
 		}
 	}
 
@@ -53,5 +82,6 @@ public class Runner : MonoBehaviour {
 
 	void OnCollisionExit () {
 		touchingPlatform = false;
-	}
+	}	
+
 }

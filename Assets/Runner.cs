2df8399c0bdcc0c9626abe692e958a77943f8d9d
Adam Proctor <aproctor@gmail.com>
2013-05-04 10:15:48 -0400

    directional light and initial runner script

---
+++ Assets/Runner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Runner : MonoBehaviour {
+	
+	public static float distanceTraveled;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update () {
+		transform.Translate(5f * Time.deltaTime, 0f, 0f);
+		distanceTraveled = transform.localPosition.x;
+	}
+}

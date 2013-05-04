dca175faca3889f4b98014cfa31bf3143be455c7
Adam Proctor <aproctor@gmail.com>
2013-05-04 12:03:32 -0400

    double jump

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -6,11 +6,38 @@ public class Runner : MonoBehaviour {
 	public static float distanceTraveled;
 
 	public float acceleration;
+	public Vector3 jumpVelocity;
+	public int maxJumps;
 
 	private bool touchingPlatform;
+	private int jumpCount = 0;
+	
+	void Start() {
+		jumpCount = 0;
+	}
 
 	void Update () {
+		if(Input.GetButtonDown("Jump")){
+			Jump();
+		}
 		distanceTraveled = transform.localPosition.x;
+	}
+	
+	private void Jump() {
+		var canJump = false;
+		if(touchingPlatform) {
+			canJump = true;
+		} else if(jumpCount < maxJumps) {
+			canJump = true;
+			//TODO super power
+		}
+		if(canJump) {
+			Debug.Log("JUMPING");
+			rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+			jumpCount += 1;
+		} else {
+			Debug.Log("D'Oh");
+		}
 	}
 
 	void FixedUpdate () {
@@ -21,6 +48,7 @@ public class Runner : MonoBehaviour {
 
 	void OnCollisionEnter () {
 		touchingPlatform = true;
+		jumpCount = 0;
 	}
 
 	void OnCollisionExit () {

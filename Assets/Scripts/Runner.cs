93e7ba59cbd5c0859c54e04a8d85b85d61b9d98b
Adam Proctor <aproctor@gmail.com>
2013-05-04 11:24:22 -0400

    platform friction

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -2,16 +2,28 @@ using UnityEngine;
 using System.Collections;
 
 public class Runner : MonoBehaviour {
-	
+
 	public static float distanceTraveled;
-	
-	// Use this for initialization
-	void Start () {
-	
-	}
+
+	public float acceleration;
+
+	private bool touchingPlatform;
 
 	void Update () {
-		transform.Translate(5f * Time.deltaTime, 0f, 0f);
 		distanceTraveled = transform.localPosition.x;
+	}
+
+	void FixedUpdate () {
+		if(touchingPlatform){
+			rigidbody.AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
+		}
+	}
+
+	void OnCollisionEnter () {
+		touchingPlatform = true;
+	}
+
+	void OnCollisionExit () {
+		touchingPlatform = false;
 	}
 }
